f18364d33536835e2782ad40fe983fc145d6683a
Paul Mucur <mudge@mudge.name>
2016-11-17 22:20:08 +0000

    Print metadata for every operation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#[macro_use(bson, doc)]
 extern crate bson;
 extern crate mongodb;
 
@@ -13,17 +12,53 @@ fn main() {
 
     let coll = client.db("local").collection("oplog.rs");
 
+    let mut opts = FindOptions::new();
+    opts.cursor_type = CursorType::TailableAwait;
+    opts.no_cursor_timeout = true;
+
+    let mut cursor = coll.find(None, Some(opts)).expect("Failed to execute find");
+
     loop {
-        let mut opts = FindOptions::new();
-        opts.cursor_type = CursorType::TailableAwait;
-        opts.no_cursor_timeout = true;
-        let cursor = coll.find(None, Some(opts)).expect("Failed to execute find");
-        for result in cursor {
-            if let Ok(item) = result {
-                if let Some(&Bson::String(ref op)) = item.get("op") {
-                    println!("op: {}", op);
-                }
-            }
+        let item = cursor.next();
+
+        match item {
+            Some(Ok(doc)) => log_operation(doc),
+            Some(Err(_)) => panic!("Failed to get next from server!"),
+            None => println!("Reached end of oplog"),
         }
     }
+}
+
+fn log_operation(doc: bson::Document) {
+    let op = match doc.get("op") {
+        Some(&Bson::String(ref op)) => op,
+        _ => panic!("Expected op to be a string!"),
+    };
+
+    let ns = match doc.get("ns") {
+        Some(&Bson::String(ref ns)) => ns,
+        _ => panic!("Expected ns to be a string!"),
+    };
+
+    let v = match doc.get("v") {
+        Some(&Bson::I32(ref v)) => v,
+        _ => panic!("Expected v to be a 32-bit integer!"),
+    };
+
+    let h = match doc.get("h") {
+        Some(&Bson::I64(ref h)) => h,
+        _ => panic!("Expected h to be a 64-bit integer!"),
+    };
+
+    let ts = match doc.get("ts") {
+        Some(&Bson::TimeStamp(ref ts)) => ts,
+        _ => panic!("Expected ts to be a timestamp!"),
+    };
+
+    let o = match doc.get("o") {
+        Some(&Bson::Document(ref o)) => o,
+        _ => panic!("Expected o to be a document!"),
+    };
+
+    println!("op: {} ns: {} v: {} h: {} ts: {} o: {}", op, ns, v, h, ts, o);
 }
